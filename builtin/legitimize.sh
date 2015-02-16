57c972593c8e965c6bb45f262f8c421dee1d0f42
Emily Shepherd <emily@emilyshepherd.me>
2015-02-16 20:04:27 +0000

    Several improvements to legitimize.sh:
    + Added guards to the file creations
    + Will now stash uncommitted files for setup (and pop them after)
    + If master was empty it will recreate it afterwards

--- builtin/legitimize.sh
+++ builtin/legitimize.sh
@@ -1,30 +1,65 @@
 #!/bin/sh
 
-exists=`git show-ref refs/heads/tracking`
+orig_head=`git symbolic-ref -q --short HEAD`
+stashed=0
 
-if [ ! -n "$exists" ]
-    then
-
-    current_branch=`git symbolic-ref -q --short HEAD`
-
+if ! git show-ref refs/heads/tracking; then
     echo "Creating tracking branch..."
     git checkout --orphan tracking
 
     echo "Cleaning workspace..."
     git rm --force --quiet -r .
+else
+    if git diff-index --quiet HEAD; then
+        git stash
+        stashed = 1
+    fi
 
-    echo "Initializing tracking directory..."
+    git checkout tracking
+fi
+
+echo "Initializing tracking directory..."
+if [ ! -d .tracking/ ]
+    then
     mkdir .tracking/
-    cd .tracking/
-    mkdir users/
+fi
+cd .tracking/
+
+if [ ! -a config ]
+    then
+    touch config
+fi
+
+if [ ! -d proposals/ ]
+    then
     mkdir proposals/
-    touch proposals/open
-    touch proposals/pending
+fi
+cd proposals/
 
-    cd ..
-    git add .tracking/ > /dev/null
-    git commit -m 'Initialized .tracking branch' > /dev/null
+if [ ! -a open ]
+    then
+    touch open
+fi
+if [ ! -a pending ]
+    then
+    touch pending
+fi
 
-    echo "Restoring workspace..."
-    git checkout $current_branch > /dev/null
+cd ../..
+
+git add .tracking/ > /dev/null
+git commit -m 'Initialized .tracking branch' > /dev/null
+
+echo "Restoring workspace..."
+
+if ! git show-ref refs/heads/$orig_head; then
+    git checkout --orphan $orig_head
+
+    git rm --force --quiet -r .
+else
+    git checkout $orig_head > /dev/null
+
+    if stashed; then
+        git stash pop
+    fi
 fi
